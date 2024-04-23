72fccf2fd177b13be8af07ca409c8f8d4dff2963
R.dehghani <rdehghani.akorn@gmail.com>
2024-04-23 09:03:50 +0330

    feat: create test class

--- Futsal.Migrations/obj/Debug/net6.0/Futsal.Migrations.AssemblyInfo.cs
+++ Futsal.Migrations/obj/Debug/net6.0/Futsal.Migrations.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Futsal.Migrations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6693d1e33271c83ec8bc88f562df584a8af97c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Futsal.Migrations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Futsal.Migrations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
